d6cec8bf7c707e050c919fec432342161da8bafd
Arunendra Kumar <arunendrakumar1996@gmail.com>
2019-07-16 23:26:26 +0530

    deleted

--- code/pmrs/src/main/java/com/pmrs/pojos/Group.java
+++
@@ -1,9 +0,0 @@
-package com.pmrs.pojos;
-
-import java.time.LocalDateTime;
-
-public class Group {
-	private int groupId;//g_id;
-	private int groupLeaderId;//g_lid(leader);
-	private LocalDateTime groupCreated;//g_created;
-}
