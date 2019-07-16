8ca8fbef8a80cb31bea4b81607bf843a828e1efc
Arunendra Kumar <arunendrakumar1996@gmail.com>
2019-07-16 18:59:39 +0530

    Update Group.java

--- code/pmrs/src/main/java/com/pmrs/pojos/Group.java
+++ code/pmrs/src/main/java/com/pmrs/pojos/Group.java
@@ -3,8 +3,7 @@ package com.pmrs.pojos;
 import java.time.LocalDateTime;
 
 public class Group {
-	private int g_id;
-	private int g_lid(leader);
-	private LocalDateTime g_created;
-
+	private int groupId;//g_id;
+	private int groupLeaderId;//g_lid(leader);
+	private LocalDateTime groupCreated;//g_created;
 }
