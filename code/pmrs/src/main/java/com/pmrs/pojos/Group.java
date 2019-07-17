a727d551811f256c57fe17c32f59ba129cb013e4
Arunendra Kumar <arunendrakumar1996@gmail.com>
2019-07-17 09:59:17 +0530

    changes done
    Co-Authored-By: ramandeep <ramandee@users.noreply.github.com>

--- code/pmrs/src/main/java/com/pmrs/pojos/Group.java
+++ code/pmrs/src/main/java/com/pmrs/pojos/Group.java
@@ -2,8 +2,20 @@ package com.pmrs.pojos;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.*;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "groups")
 public class Group {
-	private int groupId;//g_id;
+	
+	@Column(name = "g_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer groupId;//g_id;
+	
+	@Column
 	private int groupLeaderId;//g_lid(leader);
+	@Column(name = "g_created")
 	private LocalDateTime groupCreated;//g_created;
 }
