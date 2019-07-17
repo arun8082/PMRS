d02e661c655097384c6bd193ad04db07be5c501b
ShiprAgarwal <shprgrwl@gmail.com>
2019-07-17 15:32:09 +0530

    Department.java creation

---
+++ code/pmrs/src/main/java/com/pmrs/pojos/Department.java
@@ -0,0 +1,11 @@
+package com.pmrs.pojos;
+
+import java.time.LocalDateTime;
+
+public class Department {
+
+	private int departmentId;
+	private String departmentName;
+	private LocalDateTime departmentCreated;
+	private  //d_status
+}
