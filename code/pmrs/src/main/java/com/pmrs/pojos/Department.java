7cc50c4bfa2485fa29fa0cc9b6e2d9024b20e456
ShiprAgarwal <shprgrwl@gmail.com>
2019-07-17 15:52:06 +0530

    ..

--- code/pmrs/src/main/java/com/pmrs/pojos/Department.java
+++ code/pmrs/src/main/java/com/pmrs/pojos/Department.java
@@ -2,10 +2,21 @@ package com.pmrs.pojos;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.*;
+
+@Entity
 public class Department {
 
-	private int departmentId;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer departmentId;
+	
+	@Column(name="d_name", unique=true)
 	private String departmentName;
+	
+	@Column(name="d_created")
 	private LocalDateTime departmentCreated;
-	private  //d_status
+	
+	@Column(name="d_status")
+	private Status departmentStatus;
 }
