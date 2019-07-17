25d9c0834caa2bf3af4481c23b1cde2cf12c3cac
ramandeep <51893483+ramandee@users.noreply.github.com>
2019-07-17 08:55:52 -0400

    annotation done on groupStudent and phaseStatus..final.

--- code/pmrs/src/main/java/com/pmrs/pojos/GroupStudent.java
+++ code/pmrs/src/main/java/com/pmrs/pojos/GroupStudent.java
@@ -1,7 +1,61 @@
 package com.pmrs.pojos;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "group_students")
 public class GroupStudent {
-	private int groupId;//g_id
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "g_id", nullable = false)
+    private Integer groupId;//g_id
+	
+	@Column(name = "sid", nullable = false)
 	private int studentId;//sid
+	
+	@Column(name = "role", nullable = false)
 	private int studentRole;//role
+	
+	
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
+
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+
+	public int getStudentRole() {
+		return studentRole;
+	}
+
+
+	public void setStudentRole(int studentRole) {
+		this.studentRole = studentRole;
+	}
+
+
+	public GroupStudent()
+	{
+		System.out.println("In groupStudent defualt constructor");
+	}
+	
 }
--- code/pmrs/src/main/java/com/pmrs/pojos/PhaseStatus.java
+++ code/pmrs/src/main/java/com/pmrs/pojos/PhaseStatus.java
@@ -1,8 +1,73 @@
 package com.pmrs.pojos;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "phase_status")
 public class PhaseStatus {
-	private int phaseStatusId;// ps_id
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ps_id", nullable = false)
+	private Integer phaseStatusId;// ps_id
+	
+
 	private int projectId;//ps_pid
+	
+
 	private int phaseId;//ps_phid
-	private int phaseStatus;//ps_status	
+	
+	
+
+	@Column(name = "ps_status", nullable = false)
+	private EProjectStatus phaseStatus;//ps_status	
+	
+	public Integer getPhaseStatusId() {
+		return phaseStatusId;
+	}
+
+
+	public void setPhaseStatusId(Integer phaseStatusId) {
+		this.phaseStatusId = phaseStatusId;
+	}
+
+
+	public int getProjectId() {
+		return projectId;
+	}
+
+
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+
+
+	public int getPhaseId() {
+		return phaseId;
+	}
+
+
+	public void setPhaseId(int phaseId) {
+		this.phaseId = phaseId;
+	}
+
+
+
+	
+	public EProjectStatus getPhaseStatus() {
+		return phaseStatus;
+	}
+
+
+	public void setPhaseStatus(EProjectStatus phaseStatus) {
+		this.phaseStatus = phaseStatus;
+	}
+
+
+	public PhaseStatus()
+	{
+		System.out.println("In default phase status constructor");
+	}
 }
