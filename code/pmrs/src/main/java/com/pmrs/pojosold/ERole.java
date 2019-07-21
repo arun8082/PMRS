07411c552e92ed2cfedfbbc83d6e63d38389095d
Arunendra Kumar <arunendrakumar1996@gmail.com>
2019-07-21 10:16:11 +0530

    new pojo  package created

--- code/pmrs/src/main/java/com/pmrs/pojosold/ERole.java
+++ code/pmrs/src/main/java/com/pmrs/pojosold/ERole.java
@@ -1,4 +1,4 @@
-package com.pmrs.pojos;
+package com.pmrs.pojosold;
 
 public enum ERole {
 	ADMIN, MENTOR, STUDENT
--- code/pmrs/src/main/java/com/pmrs/pojosold/Student.java
+++ code/pmrs/src/main/java/com/pmrs/pojosold/Student.java
@@ -1,4 +1,4 @@
-package com.pmrs.pojos;
+package com.pmrs.pojosold;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
