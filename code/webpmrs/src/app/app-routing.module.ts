3e084ba420dbf6e65dd9218d3d20fca926052a9c
shash002 <52948313+shash002@users.noreply.github.com>
2019-07-23 17:38:14 +0530

    about us changed
    Co-Authored-By: shash002 <shash002@users.noreply.github.com>

--- code/webpmrs/src/app/app-routing.module.ts
+++ code/webpmrs/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './header/Login/Login.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 
 const routes: Routes = [
-  {path:"login",component:LoginComponent}
+  {path:"login",component:LoginComponent},
+  {path:"aboutus",component:AboutusComponent}
+
+
 ];
 
 @NgModule({
