fed1bbf3775f2847c59e8583a56f84a98ba5b2e0
ramandeep <51893483+ramandee@users.noreply.github.com>
2019-07-26 04:47:04 -0400

    login validation changed

--- code/webpmrs/src/app/modules/Login/Login.component.ts
+++ code/webpmrs/src/app/modules/Login/Login.component.ts
@@ -19,8 +19,7 @@ import { LoginFields } from './LoginInterface';
            this.LoginForm = new FormGroup(
                {
                 username: new FormControl('',[Validators.required,Validators.maxLength(100)]),
-                password: new FormControl('',[Validators.maxLength(6),
-                    Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')])
+                password: new FormControl('',[ Validators.required,Validators.minLength(6),Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')])
                
                 });
        }
