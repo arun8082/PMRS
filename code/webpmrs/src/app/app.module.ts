ebd380e3626ac73cee503c3cefa222ad930fe6df
ramandeep <51893483+ramandee@users.noreply.github.com>
2019-07-26 09:24:41 -0400

    student list loation changed

--- code/webpmrs/src/app/app.module.ts
+++ code/webpmrs/src/app/app.module.ts
@@ -21,8 +21,8 @@ import { HeaderComponent } from './shared/header/header.component';
 import { LoginComponent } from './modules/Login/Login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AdmindashboardComponent } from './modules/admindashboard/admindashboard.component';
-import { StudentListComponent } from './student-list/student-list.component';
 import { MentordashboardComponent } from './modules/mentordashboard/mentordashboard.component';
+import { StudentListComponent } from './modules/student-list/student-list.component';
 
 
 @NgModule({
