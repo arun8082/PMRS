f5cad2d37769123897e580418fc500f3def9d16f
ashu6397 <ashutoshvaish6397@gmail.com>
2019-07-29 11:11:15 +0530

    project repository

--- code/webpmrs/src/app/modules/projectrepository/projectrepository.component.ts
+++ code/webpmrs/src/app/modules/projectrepository/projectrepository.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/core/services/common.service';
+import { Http } from '@angular/http';
+import { Project } from 'src/app/core/models/Project';
 
 @Component({
   selector: 'app-projectrepository',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectrepositoryComponent implements OnInit {
 
-  constructor() { }
+  listrepository:Project;
+  index=1; 
+  error:string;
+  projectList: any;
+  contentTitle;
+
+  constructor(private http:Http ,private commonService: CommonService) { }
 
   ngOnInit() {
+
+    this.getProjectList();
   }
 
+    baseurl="http://localhost:7090/pmrs/admin/projectList";
+     getProjectList() {
+      this.http.get(this.baseurl).subscribe(result => {
+        if (result.text() != "") {
+          this.contentTitle="Project List";
+          this.projectList = result.json();        
+        } else {
+          this.error = "There is no result...";
+        }
+      });
+
+    }
 }
